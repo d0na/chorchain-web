d150c6482790427da55b68d8927998f018e1e8da
Francesco <francesco.donini@iit.cnr.it>
2019-10-25 10:42:14 +0200

    Added Deploy component to the route

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,13 +2,16 @@ import {Modeler} from "./Modeler/Modeler";
 import {Home} from "./Home";
 import React from "react";
 import {Route, Switch} from "react-router-dom";
+import {Deploy} from "./Deploy/Deploy";
 
 export function Routes() {
 
     return (
         <Switch>
             <Route path="/dapp"/>
-            <Route path="/deploy"/>
+            <Route path="/deploy">
+                <Deploy/>
+            </Route>
             <Route path="/design">
                 <Modeler/>
             </Route>
